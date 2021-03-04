fd0050ed41998980483a5a8dcab93a613b3c18b6
Felix Steiner <felix.steiner7@gmail.com>
2021-03-04 15:00:59 +0100

    Adjusted error-handling of the API

--- api-service/routes.js
+++ api-service/routes.js
@@ -4,7 +4,7 @@ const db = require('./database');
 
 router.get('/contact', (req, res, next) => {
   db.query('SELECT * FROM contact;', (error, results) => {
-    if (error) throw error;
+    if (error) next(error);
     res.status(200).json(results.rows);
   });
 });
@@ -12,7 +12,7 @@ router.get('/contact', (req, res, next) => {
 router.get('/contact/:id', (req, res, next) => {
   const { id } = req.params;
   db.query('SELECT * FROM contact WHERE id = $1;', [id], (error, results) => {
-    if (error) throw error;
+    if (error) next(error);
     res.status(200).json(results.rows);
   });
 });
@@ -23,7 +23,7 @@ router.post('/contact', (req, res, next) => {
     'INSERT INTO contact (firstname, lastname, email, phone, birthdate) VALUES($1, $2, $3, $4, $5) RETURNING *;',
     [firstname, lastname, email, phone, birthdate],
     (error, results) => {
-      if (error) throw error;
+      if (error) next(error);
       res.status(201).json(results.rows);
     }
   );
@@ -36,7 +36,7 @@ router.put('/contact/:id', (req, res, next) => {
     'UPDATE contact SET firstname = $1, lastname = $2, email = $3, phone = $4, birthdate = $5 WHERE id = $6 RETURNING *;',
     [firstname, lastname, email, phone, birthdate, id],
     (error, results) => {
-      if (error) throw error;
+      if (error) next(error);
       res.status(200).json(results.rows);
     }
   );
@@ -45,7 +45,7 @@ router.put('/contact/:id', (req, res, next) => {
 router.delete('/contact/:id', (req, res, next) => {
   const { id } = req.params;
   db.query('DELETE FROM contact WHERE id = $1 RETURNING *', [id], (error, results) => {
-    if (error) throw error;
+    if (error) next(error);
     res.status(200).json(results.rows);
   });
 });
