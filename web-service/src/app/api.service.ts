5e37dea789d226f552e41c2755b7b307ea13bf54
Felix Steiner <felix.steiner7@gmail.com>
2021-03-04 12:31:02 +0100

    Adjusted API service so it returns oberservables

--- web-service/src/app/api.service.ts
+++ web-service/src/app/api.service.ts
@@ -1,49 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Contact } from './contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  contacts = [
-    { id: 1, firstname: 'Felix', lastname: 'Steiner', email: 'felix.steiner7@gmail.com', phone: '0699 11311258' }
-  ]
   host = `localhost:8080`;
   contactsEndpoint = `${this.host}/contacts`
 
   constructor(private httpClient: HttpClient) { }
 
-  createContact(contact: Contact) {
-    this.httpClient.post<Contact>(this.contactsEndpoint, contact)
-      .subscribe(
-        data => console.log(data),
-        error => console.log(error)
-      );
+  createContact(contact: Contact): Observable<Contact> {
+    return this.httpClient.post<Contact>(this.contactsEndpoint, contact);
   }
 
-  readContacts() {
-    // this.httpClient.get<Contact[]>(this.contactsEndpoint)
-    //   .subscribe(
-    //     data => console.log(data),
-    //     error => console.log(error)
-    //   );
-    return this.contacts;
+  readContacts(): Observable<Contact[]> {
+    return this.httpClient.get<Contact[]>(this.contactsEndpoint);
   }
 
-  updateContact(id: number, contact: Contact) { 
-    this.httpClient.put<Contact>(`${this.contactsEndpoint}/${id}`, contact)
-      .subscribe(
-        data => console.log(data),
-        error => console.log(error)
-      );
+  updateContact(id: number, contact: Contact): Observable<Contact> { 
+    return this.httpClient.put<Contact>(`${this.contactsEndpoint}/${id}`, contact);
   }
 
-  deleteContact(id: number) { 
-    this.httpClient.delete<Contact>(`${this.contactsEndpoint}/${id}`)
-      .subscribe(
-        data => console.log(data),
-        error => console.log(error)
-      );
+  deleteContact(id: number): Observable<Contact> { 
+    return this.httpClient.delete<Contact>(`${this.contactsEndpoint}/${id}`);
   }
 }
