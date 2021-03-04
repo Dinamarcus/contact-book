de61f8286004ca29daf4715f73111eedaa0858d9
Felix Steiner <felix.steiner7@gmail.com>
2021-03-04 13:12:21 +0100

    Adjusted naming for the SQL setup-script

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE contact (
    id serial PRIMARY KEY,
-   first_name VARCHAR ( 50 ) NOT NULL,
-   last_name VARCHAR ( 50 ) NOT NULL,
+   firstname VARCHAR ( 50 ) NOT NULL,
+   lastname VARCHAR ( 50 ) NOT NULL,
    email VARCHAR ( 50 ) NOT NULL,
    phone VARCHAR ( 50 ) NOT NULL,
    birthdate DATE NOT NULL
